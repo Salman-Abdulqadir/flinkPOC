34ae2b3ddbbef1c47443bd7f9e9ddc01d18267e3
salman-abdulqadir <salmanabdulkadir82@gmail.com>
2023-08-23 13:32:34 +0400

    cassandra connector class implemented

--- mongo_connector/src/main/java/ai/saal/K2CConnector.java
+++ mongo_connector/src/main/java/ai/saal/K2CConnector.java
@@ -1,83 +1,60 @@
 package ai.saal;
 
-import com.datastax.driver.mapping.Mapper;
+import ai.saal.utils.CassandraConnector;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.connectors.cassandra.CassandraSink;
-
+import org.apache.flink.util.Collector;
+import java.util.UUID;
+import scala.Tuple2;
 public class K2CConnector {
     public static void main(String[] args) throws Exception{
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        final String topicName = "movie_ratings";
+        final String bootstrapServer = "kafka:9092";
+        final String cassandraHost = "cassandra";
+        final int cassandraPort =  9042;
+        final String clusterName = "cluster_name";
+        final String keyspaceName = "flink_sink";
 
         // kafka source
-        KafkaSource<MovieRating> kafkaSource = KafkaSource.<MovieRating>builder()
-                .setBootstrapServers("kafka:9092")
-                .setTopics("movie_ratings")
+        KafkaSource<String> kafkaSource = KafkaSource.<String>builder()
+                .setBootstrapServers(bootstrapServer)
+                .setTopics(topicName)
                 .setStartingOffsets(OffsetsInitializer.earliest())
                 .setValueOnlyDeserializer(new SimpleStringSchema())
                 .build();
 
+
+        // create the table with the topic name if not exist, and that table have one json column which is "data"
+        final CassandraConnector cassandraConnector = new CassandraConnector(cassandraHost, cassandraPort);
+        cassandraConnector.createTable(topicName, keyspaceName);
+
         // cassandra sink
-        DataStream<MovieRating> stream = env.fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "Kafka-Source");
+        DataStream<Tuple2<UUID, String>> stream = env
+                .fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "Kafka-Source")
+                .flatMap(new FlatMapFunction<String, Tuple2<UUID, String>>() {
+                    @Override
+                    public void flatMap(String s, Collector<Tuple2<UUID, String>> collector) throws Exception {
+                        collector.collect(new Tuple2<>(UUID.randomUUID(), s));
+                    }
+                })
+                .keyBy(value -> value._1());
+        // processing the stream, and the make the data in a json format
         CassandraSink.addSink(stream)
-                .setQuery("INSERT INTO flink_poc.movie_ratings (id, movie, user_id, rating, timestamp) VALUES (uuid(), ?,?,?,?)")
-                .setHost("127.0.0.1")
+                .setHost(cassandraHost)
+                .setQuery(String.format("INSERT INTO %s.%s (id, data) VALUES (?, ?)", keyspaceName, topicName))
                 .build();
+
         env.execute("K2CConnector");
-    }
-    public class MovieRating{
-        private String movie;
-        private String user_id;
-        private float rating;
-        private String timestamp;
 
-        MovieRating(){}
-        MovieRating(String movie, String user_id, float rating, String timestamp){
-            this.movie = movie;
-            this.rating = rating;
-            this.user_id = user_id;
-            this.timestamp = timestamp;
-        }
-        public void setMovie(String movie){
-            this.movie = movie;
-        }
-        public void setRating(float rating){
-            this.rating = rating;
-        }
-        public void setUser_id(String user_id){
-            this.user_id = user_id;
-        }
-        public void setTimestamp (String timestamp){
-            this.timestamp = timestamp;
-        }
-
-        // getters
-        public String getMovie(){
-            return this.movie;
-        }
-        public String getUser_id(){
-            return this.user_id;
-        }
-        public String getTimestamp(){
-            return this.timestamp;
-        }
-        public float getRating(){
-            return this.rating;
-        }
-
-        @Override
-        public String toString(){
-            return "MovieRating{" +
-                    ", movie='" + movie + '\'' +
-                    ", user_id='" + user_id + '\'' +
-                    ", rating=" + rating +
-                    ", timestamp=" + timestamp +
-                    '}';
-        }
+        // closing the connection when the flink job finishes
+        cassandraConnector.close();
 
     }
 }
